0aabee629fda34c9406d1675be08bdbc417b42f8
Tim Skaggs <me@timskaggs.com>
2017-08-17 10:24:25 -0600

    Add Login index, migrate login structure over, add simple animation

--- App.js
+++ App.js
@@ -1,15 +1,28 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-import Splash from './Splash';
-import Login from './src/components/Login/Login';
+import Splash from './src/components/Splash/Splash';
+import AuthScreen from './src/components/Login';
 
 export default class App extends React.Component {
   constructor(props){
     super(props)
     this.state = {
-      timePassed: false
+      timePassed: false,
+      isLoggedIn: false, // Is the user authenticated?
+      isLoading: false, // Is the user loggingIn/signinUp?
+      isAppReady: false // Has the app completed the login animation?
     }
+  }
+
+  _simulateLogin = (username, password) => {
+    this.setState({ isLoading: true })
+    setTimeout(() => this.setState({ isLoggedIn: true, isLoading: false }), 1000)
+  }
+
+  _simulateSignup = (username, password, fullName) => {
+    this.setState({ isLoading: true })
+    setTimeout(() => this.setState({ isLoggedIn: true, isLoading: false }), 1000)
   }
 
   componentDidMount() {
@@ -23,23 +36,31 @@ export default class App extends React.Component {
   }
 
   render() {
+    // if (this.state.isAppReady) {
+    //   return (
+    //     <HomeScreen
+    //       logout={() => this.setState({ isLoggedIn: false, isAppReady: false })}
+    //     />
+    //   )
+    // } else {
+    //   return (
+    //     <AuthScreen
+    //       login={this._simulateLogin}
+    //       signup={this._simulateSignup}
+    //       isLoggedIn={this.state.isLoggedIn}
+    //       isLoading={this.state.isLoading}
+    //       onLoginAnimationCompleted={() => this.setState({ isAppReady: true })}
+    //     />
+    //   )
+    // }
     if (!this.state.timePassed){
       return (
         <Splash/>
       )
     }else{
       return (
-        <Login/>
+        <AuthScreen/>
       )
     }
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/components/Login/index.js
@@ -0,0 +1,60 @@
+import React, { Component } from 'react';
+import { StyleSheet, Text, KeyboardAvoidingView } from 'react-native';
+import { Image, View } from 'react-native-animatable';
+
+import imgLogo from '../../images/Octocat.png'
+
+import LoginForm from './LoginForm';
+
+export default class Login extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <View
+          animation={'bounceIn'}
+          duration={1200}
+          delay={200}
+          style={styles.logoContainer}>
+          <Image
+            style={styles.logo}
+            source={imgLogo} />
+        </View>
+        <KeyboardAvoidingView
+          keyboardVerticalOffset={100}
+          behavior={'padding'}
+          style={styles.formContainer}
+        >
+          <LoginForm />
+        </KeyboardAvoidingView>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#3498db'
+  },
+  logoContainer: {
+    alignItems: 'center',
+    flexGrow: 1,
+    justifyContent: 'center'
+  },
+  logo: {
+    width: 100,
+    height: 100,
+    flex: 1,
+    // height: null,
+    alignSelf: 'center',
+    resizeMode: 'contain',
+    marginVertical: 30
+  },
+  title: {
+    color: 'white',
+    marginTop: 10,
+    width: 160,
+    textAlign: 'center',
+    opacity: 0.9
+  }
+})
